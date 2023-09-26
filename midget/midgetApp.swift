469a961d950c38bf1bf6e59b8e641d381fbccf01
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-09-26 23:20:55 +0200

    Initial Commit

---
+++ midget/midgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  midgetApp.swift
+//  midget
+//
+//  Created by Arthur Fontaine on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct midgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
