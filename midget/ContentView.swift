6210a62ab3ec9babd049dfb6215ee210d9a7abce
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-02 14:37:26 +0200

    Change the app content

--- midget/ContentView.swift
+++ midget/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Version \(appVersion ?? "")")
         }
         .padding()
     }
